85c58b3f60b5e9c8c07b037784a059153e39b50e
UltraLobe <ganaslobe@gmail.com>
2023-06-12 14:02:48 +0700

    test

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -31,10 +31,9 @@ HOOKAF(void, Input, void *thiz, void *ex_ab, void *ex_ac) {
 }
 
 
-void (*old_noRecoil) (void *instance) {
+void (*old_noRecoil) (void *instance) ;
 void noRecoil (void *instance) {
 if (instance! =NULL && NoRecoil) {
-}
 }
 return old_noRecoil(instance) ;
 }
